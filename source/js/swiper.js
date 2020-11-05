ccbefb5883edf518327317475ac59f318f652f04
Milena <bobkova.milena25@gmail.com>
2020-11-05 23:39:16 +0300

    Добавлена реализация слайдера

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -3,21 +3,49 @@
 (function () {
 
   var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    slidesPerGroup: 4,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+      type: 'bullets',
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      // when window width is >= 1024px
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 4,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'bullets',
+        },
+      },
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'custom',
+          renderCustom: function (swiper, current, total) {
+            return current + ' of ' + total;
+          },
+        },
+      },
+    },
   });
-
-  swiper();
 
 })();
